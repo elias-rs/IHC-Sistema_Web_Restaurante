199f0adae94319647f4300db849d6983a9d02689
elias-rs <107665063+elias-rs@users.noreply.github.com>
2025-11-24 19:40:48 -0300

    Arquivos principais do sistema web

---
+++ js/dados.js
@@ -0,0 +1,346 @@
+var MENU = {
+    "almoco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/Almoço/bifeacebolado.jpg",
+            "name": "Bife Acebolado",
+            "dsc": "Bife Acebolado",
+            "price": 18
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/Almoço/churrasco.png",
+            "name": "Churrasco",
+            "dsc": "Churrasco",
+            "price": 18
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/Almoço/pratofeito.png",
+            "name": "Prato Feito",
+            "dsc": "Prato Feito",
+            "price": 16
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/Almoço/feijoada.png",
+            "name": "Feijoada",
+            "dsc": "Feijoada",
+            "price": 20
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/cardapio/Almoço/feijoadavegana.webp",
+            "name": "Feijoada Vegana",
+            "dsc": "Feijoada Vegana",
+            "price": 22
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/Almoço/escondidinho.jpg",
+            "name": "Escondidinho",
+            "dsc": "Escondidinho de Carne Seca",
+            "price": 16
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/Almoço/lasanha.webp",
+            "name": "Lasanha",
+            "dsc": "Lasanha Bolonhesa",
+            "price": 18
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/Almoço/filecomfritas.avif",
+            "name": "Filé com Fritas",
+            "dsc": "Filé com Fritas",
+            "price": 17
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/Almoço/omelete.webp",
+            "name": "Omelete",
+            "dsc": "Omelete",
+            "price": 12
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/Almoço/Strogonoff-de-Frango.jpg",
+            "name": "Strogonoff de Frango",
+            "dsc": "Strogonoff de Frango",
+            "price": 16
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/Almoço/tilapia-frita-limao-cr-2.jpg",
+            "name": "Tilapia Frita com Limão",
+            "dsc": "Tilapia Frita com Limão",
+            "price": 16
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/Almoço/salada.jpg",
+            "name": "Salada Completa",
+            "dsc": "Salada Completa",
+            "price": 10
+        }
+    ],
+    "cafeDaManha": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/Cafe da Manha/batatadoce.jpg",
+            "name": "Batata Doce",
+            "dsc": "Batata Doce",
+            "price": 5
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/cardapio/Cafe da Manha/bolodemacacheira.png",
+            "name": "Bolo de Macaxeira",
+            "dsc": "Bolo de Macaxeira",
+            "price": 12
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/Cafe da Manha/bolodemilho.png",
+            "name": "Bolo de Milho",
+            "dsc": "Bolo de Milho",
+            "price": 12
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/Cafe da Manha/crepe.webp",
+            "name": "Crepe Recheada",
+            "dsc": "Crepe Recheada",
+            "price": 10
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/Cafe da Manha/paodequeijo.png",
+            "name": "Pão de Queijo",
+            "dsc": "Pão de Queijo",
+            "price": 1.5
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/Cafe da Manha/cuscuz.png",
+            "name": "Cuscuz",
+            "dsc": "Cuscuz Recheado",
+            "price": 18.00
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/cardapio/Cafe da Manha/tapioca.webp",
+            "name": "Tapioca",
+            "dsc": "Tapioca Recheada",
+            "price": 8.00
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/Cafe da Manha/tapiocavegana.jpg",
+            "name": "Tapioca Vegana",
+            "dsc": "Tapioca Vegana",
+            "price": 10
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/Cafe da Manha/hamburguer-frango.png",
+            "name": "Hambúrguer Fit de Frango",
+            "dsc": "Hambúrguer Fit de Frango",
+            "price": 16
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "./img/cardapio/Cafe da Manha/lanche-natural.png",
+            "name": "Sanduíche Natural Simples",
+            "dsc": "Sanduíche Natural Simples",
+            "price": 9
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/cardapio/Cafe da Manha/sandubavegano.png",
+            "name": "Sanduíche Vegano",
+            "dsc": "Sanduíche Vegano",
+            "price": 11
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "./img/cardapio/Cafe da Manha/panquecadoce.png",
+            "name": "Panqueca Doce",
+            "dsc": "Panqueca Doce",
+            "price": 10
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/suco-de-maracuja-com-leite.jpg",
+            "name": "Suco de Maracujá com Leite",
+            "dsc": "Suco de Maracujá com Leite",
+            "price": 15
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/vitaminadeabacate.png",
+            "name": "Suco de Abacate",
+            "dsc": "Suco de Abacate",
+            "price": 15
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/bebidas/vitaminadeabacaxi.webp",
+            "name": "Suco de Abacaxi",
+            "dsc": "Suco de Abacaxi",
+            "price": 15
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/bebidas/vitaminadegoiaba.webp",
+            "name": "Suco de Goiaba",
+            "dsc": "Suco de Goiaba",
+            "price": 15
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/bebidas/vitaminadelaranja.webp",
+            "name": "Suco de Laranja",
+            "dsc": "Suco de Laranja",
+            "price": 15
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/bebidas/vitaminadelimão.png",
+            "name": "Suco de Limão",
+            "dsc": "SUco de Limão",
+            "price": 15
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/bebidas/vitamina-de-acerola.jpeg",
+            "name": "Suco de Acerola",
+            "dsc": "Suco de Acerola",
+            "price": 15
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/bebidas/vitamina-de-Banana.jpg",
+            "name": "Suco de Banana",
+            "dsc": "Suco de Banana",
+            "price": 15
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/bebidas/vitamina-de-morango.webp",
+            "name": "Suco de Morango",
+            "dsc": "Suco de Morango",
+            "price": 15
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/bebidas/vitaminadejaca.jpg",
+            "name": "Suco de Jaca",
+            "dsc": "Suco de Jaca",
+            "price": 15
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/bebidas/vitaminademanga.jpg",
+            "name": "Suco de Manga",
+            "dsc": "Suco de Manga",
+            "price": 15
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/vitamina-de-mamao.jpeg",
+            "name": "Suco de Mamão",
+            "dsc": "Suco de Mamão",
+            "price": 15
+        }
+    ],
+    "jantar": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/Jantar/batatas-recheadas.jpg",
+            "name": "Batatas Recheadas",
+            "dsc": "Batatas Recheadas",
+            "price": 12
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/Jantar/caldodemacaxeira.jpg",
+            "name": "Caldo de Macaxeira",
+            "dsc": "Caldo de Macaxeira",
+            "price": 8
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/Jantar/caldodemocoto.jpg",
+            "name": "Caldo de Mocotó",
+            "dsc": "Caldo de Mocotó",
+            "price": 8
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/Jantar/creme-de-milho-verde-44513.jpg",
+            "name": "Creme de Milho Verde",
+            "dsc": "Creme de Milho Verde",
+            "price": 10
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/Jantar/cremedecebola.jpg",
+            "name": "Creme de Cebola",
+            "dsc": "Creme de Cebola",
+            "price": 10
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/Jantar/cuscuzrecheado.jpeg",
+            "name": "Cuscuz Recheado",
+            "dsc": "Cuscuz Recheado",
+            "price": 14.50
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/Jantar/panquecasalgada.webp",
+            "name": "Panqueca Salgada",
+            "dsc": "Panqueca Salgada",
+            "price": 10
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/Jantar/sopa.jpg",
+            "name": "Sopa do Dia",
+            "dsc": "Sopa do Dia",
+            "price": 12
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/cardapio/Jantar/quiche.jpg",
+            "name": "Quiche",
+            "dsc": "Quiche",
+            "price": 14
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/Jantar/quiche de espinafre.jpeg",
+            "name": "Quiche de Espinafre",
+            "dsc": "Quiche de Espinafre",
+            "price": 15
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+            "img": "./img/cardapio/Jantar/torta de limão.jpg",
+            "name": "Torta de Limão",
+            "dsc": "Torta de Limão",
+            "price": 16
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/Jantar/torta-de-morango-capa.jpeg",
+            "name": "Torta de Morango",
+            "dsc": "Torta de Morango",
+            "price": 18
+        }
+    ],
+}
